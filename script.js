385c58424ed379e6baa710d93b6d2e2c345f9407
Tyler Robinson <139252157+CSummerChild@users.noreply.github.com>
2025-03-26 17:20:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ saveButton.addEventListener('click', async () => {
     noteInput.value = ''; // Clear note input
 
     // Send data to Google Sheets
-    const response = await fetch("Yhttps://script.google.com/macros/s/AKfycbwFZuFom8K-Q9fMdIzo4H8LxmD_YHko8PfVgpwNv0AdPUC_QyNxTIVAfpD7U31XL_gYiA/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbz4K0nYTWAzmeoQdBsyc19-LYyS3M9eKzQG4R-Xvs7XWAoyXNU1BPgW79bq3Lq6J-PUBw/exec", {
         method: "POST",
         mode: "no-cors",
         headers: { "Content-Type": "application/json" },
